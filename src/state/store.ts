7de0ec65b28bb466055fa7310ea93bff19cebc39
Jay200123 <kenken.ayonon@gmail.com>
2025-02-28 21:22:39 +0800

    add Zustand store for authentication state management

---
+++ src/state/store.ts
@@ -0,0 +1,16 @@
+import { persist, createJSONStorage } from "zustand/middleware";
+import { Store } from "@/types";
+import { createAuthApi } from "./api/auth";
+import { create } from "zustand";
+
+export const useStore = create<Store>()(
+  persist(
+    (...a) => ({
+      ...createAuthApi(...a),
+    }),
+    {
+      name: "auth",
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
