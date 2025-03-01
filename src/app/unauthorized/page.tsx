288a3d8b22a0f0d76b5e7c1c29efa8d2ef05e15d
Jay200123 <kenken.ayonon@gmail.com>
2025-03-01 21:13:00 +0800

    add unauthorized page component for handling access denial

---
+++ src/app/unauthorized/page.tsx
@@ -0,0 +1,7 @@
+export default function Unauthorized() {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <h1 className="text-2xl">Unauthorized</h1>
+    </div>
+  );
+}
