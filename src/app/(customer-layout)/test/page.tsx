69bd57540bc7682d35e6631702e57e0b8c5411f8
Jay200123 <kenken.ayonon@gmail.com>
2025-03-02 13:57:46 +0800

    implement data fetching for brands using react-query in admin page

--- src/app/(customer-layout)/test/page.tsx
+++ src/app/(customer-layout)/test/page.tsx
@@ -1,11 +1,22 @@
 "use client";
 import protectedRoutes from "@/hoc/protectedRoutes";
+import { useQuery } from "@tanstack/react-query";
+import { useStore } from "@/state/store";
 
 const adminPage = () => {
+  const { getAllBrands } = useStore();
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["brands"],
+    queryFn: getAllBrands,
+  });
+
+  console.log(data);
+
   return (
-    <>
+    <div>
       <h3>Hi there! Admin </h3>
-    </>
+    </div>
   );
 };
 
